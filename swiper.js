de8b92945d077a3c3721abbba8d2c1b511e1ef7e
tazeen zahra <106230564+Tazeenzahra12@users.noreply.github.com>
2023-05-10 17:44:40 +0500

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,54 @@
+// var swiper = new Swiper(".mySwiper", {
+//     cssMode: true,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+// //   });
+// adding the mouseover and mouseout funtion
+function change1()
+{
+document.getElementById("img1").src = "./Assetspage/img1.webp"
+
+}
+
+function change2()
+{
+    document.getElementById("img1").src = "./Assetspage/img1e.webp"
+}
+
+function change3()
+{
+document.getElementById("img2").src = "./Assetspage/img2.webp"
+
+}
+
+function change4()
+{
+    document.getElementById("img2").src = "./Assetspage/img2e.webp"
+}
+function change5()
+{
+document.getElementById("img3").src = "./Assetspage/img3.webp"
+
+}
+
+function change6()
+{
+    document.getElementById("img3").src = "./Assetspage/img3e.webp"
+}
+function change7()
+{
+document.getElementById("img4").src = "./Assetspage/img4.webp"
+
+}
+
+function change8()
+{
+    document.getElementById("img4").src = "./Assetspage/img4e.webp"
+}
